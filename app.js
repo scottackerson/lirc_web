50c5004906e6dde6b67210bd4c4963e0ca0da212
Scott Ackerson <scott.ackerson@gmail.com>
2014-05-25 16:44:37 -0500

    catalog view and poor man's pagination

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express'),
     consolidate = require('consolidate'),
     path = require('path'),
     swig = require('swig'),
-    sqlite3 = require('sqlite3').verbose();
+    sqlite3 = require('sqlite3').verbose()
 
 // Precompile templates
 var JST = {
@@ -14,7 +14,8 @@ var JST = {
     karaokeremote: swig.compileFile(__dirname + '/templates/karaokeremote.swig'),
     bysongnumber: swig.compileFile(__dirname + '/templates/bysongnumber.swig'),
     artistlist: swig.compileFile(__dirname + '/templates/artistlist.swig'),
-    songlist: swig.compileFile(__dirname + '/templates/songlist.swig')
+    songlist: swig.compileFile(__dirname + '/templates/songlist.swig'),
+    catalog: swig.compileFile(__dirname + '/templates/catalog.swig')
 };
 
 // Create app
@@ -56,7 +57,6 @@ try {
     console.log("Could not load DB");
   }
 
-
 // Get artist by alpha character
 function getArtistByCharacter(character, num_records, callback){
    var statement = "SELECT artist_name, artist_id FROM artists WHERE artist_name LIKE '"+character+"%' ORDER BY artist_name COLLATE NOCASE"
@@ -67,8 +67,7 @@ function getArtistByCharacter(character, num_records, callback){
                console.log('Error serving querying database. ' + err);
                return;
                       }
-         data = rows
-         callback(data);
+         callback(rows);
    });
 };
 
@@ -82,8 +81,7 @@ function getSongsByArtistID(artist_id, num_records, callback){
                console.log('Error serving querying database. ' + err);
                return;
                       }
-         data = rows
-         callback(data);
+         callback(rows);
    });
 };
 
@@ -97,12 +95,58 @@ function getSongsByCharacter(character, num_records, callback){
                console.log('Error serving querying database. ' + err);
                return;
                       }
-         data = rows
-         console.log(JSON.stringify(data));
-         callback(data);
+         console.log(JSON.stringify(rows));
+         callback(rows);
    });
 };
  
+// Get last n songs played
+function getSongHistory(num_records, callback){
+   var statement = "SELECT song_id, track, title, last_played FROM songs ORDER BY last_played DESC LIMIT " + num_records;
+   console.log(statement);
+   var songs = db.all(statement,
+      function(err, rows){
+         if (err){
+               console.log('Error serving querying database. ' + err);
+               return;
+                  }
+         console.log(JSON.stringify(rows));
+         callback(rows);
+   });
+};
+
+// Get most played songs
+function getMostPlayed(num_records, callback){
+   var statement = "SELECT song_id, track, title, times_played FROM songs ORDER BY times_played DESC LIMIT " + num_records;
+   console.log(statement);
+   var songs = db.all(statement,
+      function(err, rows){
+         if (err){
+               console.log('Error serving querying database. ' + err);
+               return;
+                  }
+         console.log(JSON.stringify(rows));
+         callback(rows);
+   });
+};
+
+// Get songs
+function getSongs(num_records, offset, callback){
+  var statement = "SELECT songs.track, songs.title, artists.artist_name from songs join artists on songs.artist_id = artists.artist_id ORDER BY artists.artist_name Limit 132 offset " + offset;
+  console.log(statement);
+   var songs = db.all(statement,
+      function(err, rows){
+         if (err){
+               console.log('Error serving querying database. ' + err);
+               return;
+                  }
+         console.log(JSON.stringify(rows));
+         callback(rows);
+   });
+};
+
+
+
 // Update song when played
 function updateSong(track){
   var statement = "UPDATE songs SET times_played = times_played + 1, last_played = CURRENT_TIMESTAMP where track = "+track;
@@ -110,10 +154,47 @@ function updateSong(track){
   db.run(statement);
 };
 
+function getRandomTrack(callback){
+  var statement = "SELECT track FROM songs ORDER BY RANDOM() LIMIT 1";
+  console.log(statement);
+   var track = db.all(statement,
+      function(err, rows){
+         if (err){
+               console.log('Error serving querying database. ' + err);
+               return;
+                      }
+         data = rows
+         callback(data[0].track.toString()); 
+   });
+};
+
+// Play a track
+function playTrack(track) {
+    console.log('playTrack: '+ track);
+    if (track) { 
+        var i = 0;
+        var interval = function() {
+            if (track[i]) {
+                var command = track[i];
+                lirc_node.irsend.send_once('karaoke', command[0], function() {});
+                console.log("Sent " + command);
+            } else {
+                clearInterval(interval);
+            }
+            i += 1;
+        };
+        setInterval(interval, 200);
+        updateSong(track);
+    }
+};
+
 // Web UI
 app.get('/', function(req, res) {
-    res.send(JST['index'].render({
-    }));
+  getSongHistory(5, function(data){
+    getMostPlayed(5, function(data2){
+      res.send(JST['index'].render({lastFive: data, mostPlayed: data2}))
+    });
+  });
 });
 
 app.get('/bysongnumber', function(req, res) {
@@ -124,6 +205,35 @@ app.get('/karaokeremote', function(req, res) {
     res.send(JST['karaokeremote'].render({}));
 });
 
+app.get('/artistlist', function(req, res) {
+    res.send(JST['artistlist'].render({
+    }));
+});
+
+app.get('/songlist', function(req, res) {
+    res.send(JST['songlist'].render({
+    }));
+});
+
+app.get('/catalog', function(req, res) {
+  var offset = 0;
+  var prevoffset = 0
+  var nextoffset = 0
+  if(typeof req.query.offset != 'undefined') {
+        var offset = parseInt(req.query.offset); 
+        prevoffset = offset - 132;
+        nextoffset = offset + 132;
+  } else if(typeof req.query.offset == 'undefined' || parseInt(req.query.offset) == 0)  {
+    prevoffset = 0;
+    nextoffset = offset + 132;
+  }
+  
+  getSongs(30, offset, function(data) {
+    res.send(JST['catalog'].render({songs: data, offset: offset, prevoffset: prevoffset, nextoffset: nextoffset}));
+  })
+});
+
+// Web API
 app.get('/apiartistlist', function(req, res) {
     getArtistByCharacter(req.query.char, 10, function(data){
         res.writeHead(200, { "Content-type": "application/json" });        
@@ -146,16 +256,13 @@ app.get('/apisongbycharacter', function(req, res) {
     });
 });
 
-app.get('/artistlist', function(req, res) {
-    res.send(JST['artistlist'].render({
-    }));
+app.get('/playrandomsong', function(req, res) {
+  getRandomTrack(function(data){
+    playTrack(data.toString());
+  });
 });
 
-app.get('/songlist', function(req, res) {
-    res.send(JST['songlist'].render({
-    }));
-});
-
+// POSTS
 // Send :remote/:command one time
 app.post('/remotes/:remote/:command', function(req, res) {
     lirc_node.irsend.send_once(req.params.remote, req.params.command, function() {});
@@ -183,7 +290,7 @@ app.post('/remotes/:remote/:command/send_stop', function(req, res) {
 // Parse the post into several remote signals
 app.post('/songnumber', function(req, res) {
     //read the post, parse it into several commands to be sent to lirc_node
-    if (req.body.songnumber) { 
+    /*if (req.body.songnumber) { 
       var i = 0;
       var interval = function() {
           if (req.body.songnumber[i]) {
@@ -197,15 +304,16 @@ app.post('/songnumber', function(req, res) {
       };
       setInterval(interval, 200);
       updateSong(req.body.songnumber);
-    }
+    }*/
+    playTrack(req.body.songnumber);
     res.setHeader('Cache-Control', 'no-cache');
     res.redirect('/bysongnumber'); //need to redirect from request origination.
 });
-
+ 
 app.get('/songnumber/:track', function(req, res) {
     //read the post, parse it into several commands to be sent to lirc_node
     console.log('Changing song via GET: '+req.params.track);
-    if (req.params.track) { 
+    /*if (req.params.track) { 
         var i = 0;
         var interval = function() {
             if (req.params.track[i]) {
@@ -219,10 +327,10 @@ app.get('/songnumber/:track', function(req, res) {
         };
         setInterval(interval, 200);
         updateSong(req.params.track);
-    }
+    }*/
+    playTrack(req.params.track);
     res.setHeader('Cache-Control', 'no-cache');
     res.send(200);
-    //res.redirect('/bysongnumber'); //need to redirect from request origination.
 });
 
 // Default port is 3000
